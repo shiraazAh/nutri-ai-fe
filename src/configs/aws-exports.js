ae14f0a9317e24bc24c5d90bfd4d1ea6b9c912e2
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2025-03-17 07:25:27 +0000

    Fix: Update the aws config with netlify env

--- src/configs/aws-exports.js
+++ src/configs/aws-exports.js
@@ -1,11 +1,9 @@
-console.log(import.meta.env);
-
 const awsmobile = {
     "aws_project_region": "eu-west-1",
-    "aws_cognito_identity_pool_id": "eu-west-1:ab9cded3-0eda-49ec-8fca-9b956a50bb6f",
+    "aws_cognito_identity_pool_id": import.meta.env.VITE_COGNITO_POOL,
     "aws_cognito_region": "eu-west-1",
-    "aws_user_pools_id": "eu-west-1_5YyfqQKke",
-    "aws_user_pools_web_client_id": "2ph8esc5v6okip5p1pl191tbvt",
+    "aws_user_pools_id": import.meta.env.VITE_USER_POOL,
+    "aws_user_pools_web_client_id": import.meta.env.VITE_WEB_CLIENT,
     "oauth": {},
     "aws_cognito_username_attributes": [
         "EMAIL"
